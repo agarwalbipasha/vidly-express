1c75445e07f7c8c820d06b57a68b052820d7168f
Bipasha Agarwal <agarwalbipasha@gmail.com>
2022-12-19 21:59:51 +0530

    Sorted get, put, post and delusing express

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
-const url = "http://m.vid.ly/api/ ";
-const fetch = (url) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(url));
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  const result = fetch(url)
-    .then((data) => JSON.stringify(data))
-    .then((data) => res.send(data))
-    .catch((err) => res.send(err));
-  //   res.send(result);
+app.use(express.json());
+
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" },
+];
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
 });
 
-app.listen(port, () => {
-  console.log(`Server listening to ${port}`);
+function validateGenre(genre) {
+  const schema = { name: Joi.string().min(3).required() };
+  return Joi.validate(genre, schema);
+}
+
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
 });
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(400).send(`The genre with the given ID is not found`);
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(400).send(`The genre with the given ID is not found`);
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
